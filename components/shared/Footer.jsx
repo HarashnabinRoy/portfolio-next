8cf4221947802190c301a4a98da94d90cbe85aa4
Exion <70258985+HarashnabinRoy@users.noreply.github.com>
2022-08-03 20:56:55 +0530

    Update 2

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -5,21 +5,27 @@ import { faCode, faCodeBranch , faEnvelope , faPalette,  } from '@fortawesome/fr
 import { faGithub, faTwitter, faDiscord, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'; 
 const Footer = () => {
     return(
-        <div className="text-[#5d5a5a99] flex flex-wrap justify-between mt-16 mb-8 text-xs mx-[200px] ">
-            <div className="flex gap-4">
-                <a href="" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faEnvelope } /> Contact</a>
-                <a href="https://github.com/HarashnabinRoy" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faCode } /> Github</a>
-                <a href="https://discords.com/bio/p/exion" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faDiscord } /> Discord</a>
-                <a href="https://www.linkedin.com/in/harashnabin-roy-8893731b9/" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faLinkedinIn } /> LinkedIn</a>
-                <a href="https://twitter.com/RoyHarashnabin" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faTwitter } /> Twitter</a>
+        <div>
+            <div className="text-[#5d5a5a99] flex flex-wrap justify-between mt-16 mb-4 text-xs mx-[200px] ">
+                <div className="flex gap-4">
+                    <a href="harashnabin2002@gmail.com" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faEnvelope } /> Contact</a>
+                    <a href="https://github.com/HarashnabinRoy" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faCode } /> Github</a>
+                    <a href="https://discords.com/bio/p/exion" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faDiscord } /> Discord</a>
+                    <a href="https://www.linkedin.com/in/harashnabin-roy-8893731b9/" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faLinkedinIn } /> LinkedIn</a>
+                    <a href="https://twitter.com/RoyHarashnabin" className="hover:text-[#908e8e] transition duration-200 ease-in-out" target="_blank"><FontAwesomeIcon icon={ faTwitter } /> Twitter</a>
+                </div>
+                <div className="flex gap-4">
+                    <div>
+                        <FontAwesomeIcon icon={ faPalette } /> Cyan
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={ faCodeBranch } /> v2.0.0 
+                    </div>
+                </div>
             </div>
-            <div className="flex gap-4">
-                <div>
-                    <FontAwesomeIcon icon={ faPalette } /> Greenish Blue 
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={ faCodeBranch } /> v2.0.0 
-                </div>
+
+            <div className="flex justify-center text-[#5d5a5a99] text-sm mb-4 cursor-pointer">
+                @Harashnabin Roy 2022
             </div>
         </div>
     );
